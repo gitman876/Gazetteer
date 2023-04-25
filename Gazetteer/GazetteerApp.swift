78aee6ceb887cf79840a0b9de465e0c2fc3e4e44
Seymour Rodrigues <seymourjr5@gmail.com>
2023-04-25 15:02:55 +0530

    Initial Commit

---
+++ Gazetteer/GazetteerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazetteerApp.swift
+//  Gazetteer
+//
+//  Created by Seymour Rodrigues on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct GazetteerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
